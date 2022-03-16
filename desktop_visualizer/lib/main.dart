8345eebeb15fed8354f9a3226eea54e4a4115bcb
Dudlileif <dudlileif@gmail.com>
2022-03-16 13:16:57 +0100

    Added basic Material Design

--- desktop_visualizer/lib/main.dart
+++ desktop_visualizer/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,18 +13,76 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.blueWhale,
+        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+        blendLevel: 18,
+        appBarStyle: FlexAppBarStyle.primary,
+        appBarOpacity: 0.95,
+        appBarElevation: 0,
+        transparentStatusBar: true,
+        tabBarStyle: FlexTabBarStyle.forAppBar,
+        tooltipsMatchBackground: true,
+        swapColors: false,
+        lightIsWhite: false,
+        useSubThemes: true,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use playground font, add GoogleFonts package and uncomment:
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+        subThemesData: const FlexSubThemesData(
+          useTextTheme: true,
+          fabUseShape: true,
+          interactionEffects: true,
+          bottomNavigationBarElevation: 0,
+          bottomNavigationBarOpacity: 0.95,
+          navigationBarOpacity: 0.95,
+          navigationBarMutedUnselectedText: true,
+          navigationBarMutedUnselectedIcon: true,
+          inputDecoratorIsFilled: true,
+          inputDecoratorBorderType: FlexInputBorderType.outline,
+          inputDecoratorUnfocusedHasBorder: true,
+          blendOnColors: true,
+          blendTextTheme: true,
+          popupMenuOpacity: 0.95,
+        ),
       ),
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.blueWhale,
+        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
+        blendLevel: 18,
+        appBarStyle: FlexAppBarStyle.background,
+        appBarOpacity: 0.95,
+        appBarElevation: 0,
+        transparentStatusBar: true,
+        tabBarStyle: FlexTabBarStyle.forAppBar,
+        tooltipsMatchBackground: true,
+        swapColors: false,
+        darkIsTrueBlack: false,
+        useSubThemes: true,
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        // To use playground font, add GoogleFonts package and uncomment:
+        // fontFamily: GoogleFonts.notoSans().fontFamily,
+        subThemesData: const FlexSubThemesData(
+          useTextTheme: true,
+          fabUseShape: true,
+          interactionEffects: true,
+          bottomNavigationBarElevation: 0,
+          bottomNavigationBarOpacity: 0.95,
+          navigationBarOpacity: 0.95,
+          navigationBarMutedUnselectedText: true,
+          navigationBarMutedUnselectedIcon: true,
+          inputDecoratorIsFilled: true,
+          inputDecoratorBorderType: FlexInputBorderType.outline,
+          inputDecoratorUnfocusedHasBorder: true,
+          blendOnColors: true,
+          blendTextTheme: true,
+          popupMenuOpacity: 0.95,
+        ),
+      ),
+      // If you do not have a themeMode switch, uncomment this line
+      // to let the device system mode control the theme mode:
+      // themeMode: ThemeMode.system,
+
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }

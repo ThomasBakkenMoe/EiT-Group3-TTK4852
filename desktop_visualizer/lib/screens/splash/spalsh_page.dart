8c419ffd6ef3df6e61837231d9205e66a4783ded
Dudlileif <dudlileif@gmail.com>
2022-04-05 18:51:17 +0200

    Added color theme selector, background map

--- desktop_visualizer/lib/screens/splash/spalsh_page.dart
+++ desktop_visualizer/lib/screens/splash/spalsh_page.dart
@@ -1,8 +1,12 @@
+import 'dart:ui';
+
 import 'package:desktop_visualizer/main.dart';
 import 'package:desktop_visualizer/models/city.dart';
 import 'package:desktop_visualizer/screens/credits/credits_page.dart';
 import 'package:desktop_visualizer/screens/how_to/how_to_page.dart';
 import 'package:desktop_visualizer/screens/leaflet_map/leaflet_map.dart';
+import 'package:desktop_visualizer/screens/splash/components/background_map.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,8 +20,6 @@ class SplashPage extends ConsumerStatefulWidget {
 }
 
 class _SplashPageState extends ConsumerState<SplashPage> {
-  final List<bool> isSelected = [true, false];
-
   @override
   void initState() {
     super.initState();
@@ -34,6 +36,8 @@ class _SplashPageState extends ConsumerState<SplashPage> {
 
         actions: [
           IconButton(
+            padding: const EdgeInsets.all(8),
+            tooltip: 'Light/Dark mode',
             onPressed: () {
               ref.read(themeModeProvider).setThemeMode(
                   Theme.of(context).brightness == Brightness.light
@@ -44,61 +48,103 @@ class _SplashPageState extends ConsumerState<SplashPage> {
                 ? Icons.light_mode
                 : Icons.dark_mode),
           ),
+          PopupMenuButton<int>(
+            onSelected: (int index) {
+              ref.read(schemeProvider).setThemeModeIndex(index);
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuItem<int>>[
+              for (int i = 0; i < FlexColor.schemesList.length; i++)
+                PopupMenuItem<int>(
+                  value: i,
+                  child: ListTile(
+                    leading: Icon(Icons.lens,
+                        color: Theme.of(context).brightness == Brightness.light
+                            ? FlexColor.schemesList[i].light.primary
+                            : FlexColor.schemesList[i].dark.primary,
+                        size: 35),
+                    title: Text(FlexColor.schemesList[i].name),
+                  ),
+                )
+            ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Icon(
+                    Icons.lens,
+                    color: Theme.of(context).brightness == Brightness.light
+                        ? Colors.white
+                        : Theme.of(context).appBarTheme.backgroundColor,
+                    size: 40,
+                  ),
+                  Icon(
+                    Icons.color_lens,
+                    color: Theme.of(context).primaryColor,
+                    size: 30,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
       body: Container(
         alignment: Alignment.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: Stack(
           children: [
-            Text(
-              'Fantastic name of application',
-              style: Theme.of(context).textTheme.headline1,
-            ),
-            Divider(indent: 300, endIndent: 300),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => LeafletMap(
-                      cities: widget.cities,
-                    ),
+            const BackgroundMap(),
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Fantastic name of application',
+                    style: Theme.of(context).textTheme.headline1,
                   ),
-                );
-              },
-              child: Text('Start'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HowToPage(),
+                  Divider(indent: 300, endIndent: 300),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LeafletMap(
+                            cities: widget.cities,
+                          ),
+                        ),
+                      );
+                    },
+                    child: Text('Start'),
                   ),
-                );
-              },
-              child: Text('How it works'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CreditsPage(),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HowToPage(),
+                        ),
+                      );
+                    },
+                    child: Text('How it works'),
                   ),
-                );
-              },
-              child: Text('Credits'),
-            )
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CreditsPage(),
+                        ),
+                      );
+                    },
+                    child: Text('Credits'),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
-        /* decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/graphics/placeholder.png'),
-              fit: BoxFit.cover),
-        ), */
       ),
     );
   }

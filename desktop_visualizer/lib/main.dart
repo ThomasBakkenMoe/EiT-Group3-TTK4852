f26ef2058bb250ef8335273b80b2858aeb9c7313
Dudlileif <dudlileif@gmail.com>
2022-04-05 18:18:21 +0200

    Simplified widget tree

--- desktop_visualizer/lib/main.dart
+++ desktop_visualizer/lib/main.dart
@@ -1,9 +1,8 @@
+import 'package:desktop_visualizer/app/satreelight.dart';
 import 'package:desktop_visualizer/models/city.dart';
-import 'package:desktop_visualizer/screens/splash/spalsh_page.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 late List<City> cities;
 
@@ -17,13 +16,11 @@ Future<void> main() async {
 
   runApp(
     ProviderScope(
-      child: MyApp(),
+      child: SaTreeLight(cities: cities),
     ),
   );
 }
 
-// Helpful website for theming
-// https://rydmike.com/flexcolorschemeV4Tut5/#/
 class ThemeModeNotifier extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
   ThemeMode get themeMode => _themeMode;
@@ -46,89 +43,5 @@ class SchemeNotifier extends ChangeNotifier {
   void setThemeModeIndex(int index) {
     _scheme = FlexScheme.values[index];
     notifyListeners();
-  }
-}
-
-class MyApp extends ConsumerWidget {
-  MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
-  final fontFamily = GoogleFonts.notoSansMono().fontFamily;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final FlexScheme scheme = ref.watch(schemeProvider).scheme;
-    final ThemeMode themeMode = ref.watch(themeModeProvider).themeMode;
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: FlexThemeData.light(
-        scheme: scheme,
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 18,
-        appBarStyle: FlexAppBarStyle.primary,
-        appBarOpacity: 0.95,
-        appBarElevation: 0,
-        transparentStatusBar: true,
-        tabBarStyle: FlexTabBarStyle.forAppBar,
-        tooltipsMatchBackground: true,
-        swapColors: false,
-        lightIsWhite: false,
-        useSubThemes: true,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        fontFamily: fontFamily,
-        subThemesData: const FlexSubThemesData(
-          useTextTheme: true,
-          fabUseShape: true,
-          interactionEffects: true,
-          bottomNavigationBarElevation: 0,
-          bottomNavigationBarOpacity: 0.95,
-          navigationBarOpacity: 0.95,
-          navigationBarMutedUnselectedText: true,
-          navigationBarMutedUnselectedIcon: true,
-          inputDecoratorIsFilled: true,
-          inputDecoratorBorderType: FlexInputBorderType.outline,
-          inputDecoratorUnfocusedHasBorder: true,
-          blendOnColors: true,
-          blendTextTheme: true,
-          popupMenuOpacity: 0.95,
-        ),
-      ),
-      darkTheme: FlexThemeData.dark(
-        scheme: scheme,
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 18,
-        appBarStyle: FlexAppBarStyle.background,
-        appBarOpacity: 0.95,
-        appBarElevation: 0,
-        transparentStatusBar: true,
-        tabBarStyle: FlexTabBarStyle.forAppBar,
-        tooltipsMatchBackground: true,
-        swapColors: false,
-        darkIsTrueBlack: false,
-        useSubThemes: true,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        fontFamily: fontFamily,
-        subThemesData: const FlexSubThemesData(
-          useTextTheme: true,
-          fabUseShape: true,
-          interactionEffects: true,
-          bottomNavigationBarElevation: 0,
-          bottomNavigationBarOpacity: 0.95,
-          navigationBarOpacity: 0.95,
-          navigationBarMutedUnselectedText: true,
-          navigationBarMutedUnselectedIcon: true,
-          inputDecoratorIsFilled: true,
-          inputDecoratorBorderType: FlexInputBorderType.outline,
-          inputDecoratorUnfocusedHasBorder: true,
-          blendOnColors: true,
-          blendTextTheme: true,
-          popupMenuOpacity: 0.95,
-        ),
-      ),
-      // If you do not have a themeMode switch, uncomment this line
-      // to let the device system mode control the theme mode:
-      themeMode: themeMode,
-
-      home: SplashPage(cities: cities),
-    );
   }
 }

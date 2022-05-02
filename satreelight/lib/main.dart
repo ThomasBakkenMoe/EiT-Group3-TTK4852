b0dd1f180ddbac3b3bea8db8882fbe6f54aa0203
Dudlileif <dudlileif@gmail.com>
2022-05-02 20:58:50 +0200

    Sorted imports, made cities local

--- satreelight/lib/main.dart
+++ satreelight/lib/main.dart
@@ -1,13 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:satreelight/app/satreelight.dart';
 import 'package:satreelight/models/city.dart';
 import 'package:satreelight/notifiers/notifiers.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-late List<City> cities;
 
 final themeModeProvider = ChangeNotifierProvider((ref) => ThemeModeNotifier());
-final schemeProvider = ChangeNotifierProvider((ref) => SchemeNotifier());
+final colorSchemeProvider =
+    ChangeNotifierProvider((ref) => ColorSchemeNotifier());
 final mapInBackgroundProvider =
     ChangeNotifierProvider((ref) => MapBackgroundNotifier());
 final mapZoomInProvider = ChangeNotifierProvider((ref) => MapZoomInNotifier());
@@ -17,7 +16,7 @@ final mapZoomOutProvider =
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  cities = await loadCities();
+  final cities = await loadCities();
 
   runApp(
     ProviderScope(
